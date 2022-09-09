1660c986cfd30ec37b04542c24bccf2f9be0215c
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-09 04:31:52 -0700

    Print numbers less than 10 using the for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	/* print all numbers less than 10 in base 10, starting from 0 */
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d\n", x);
+	}
+	putchar('\n');
+
+	return (0);
+}
