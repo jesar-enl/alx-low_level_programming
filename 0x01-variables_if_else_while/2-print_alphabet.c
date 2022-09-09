4d9f945dd216ec84bd9f8af25d7dcf1f7b0d3e68
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-09 02:43:21 -0700

    for loop used to create the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+		putchar('\n');
+	}
+	return (0);
+}
