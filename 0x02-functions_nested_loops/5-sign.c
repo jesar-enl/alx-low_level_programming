6c9d18e9f25b8208fd654ac17259286e8eba8247
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-13 06:39:37 -0700

    Sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of an integer
+ * @n: To be evaluated
+ *
+ * Description: 1 (greater than 0)
+ * 0 (is zero) -1 (less than 0)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('+');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
