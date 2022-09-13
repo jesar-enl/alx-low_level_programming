43cf4fe9ecbef4a1ffe262dd1b4d3b3445bbfa6d
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-13 06:07:30 -0700

    Lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,14 @@
  * _isalpha - checks for lowercase and uppercase
  * @c: character to be evaluated
  *
- * Description: Defines whether the character is 
+ * Description: Defines whether the character is
  * lowercase or uppercase
  * Return: 0 (if failed), 1 (if success)
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122 ) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
