f1a67fa10f995bbf33a556df1f6f25c73da8aa0c
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-14 02:29:13 -0700

    Debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - tests whether the function prints
+ * an integer as positive or negative
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 97;
 	positive_or_negative(i);
 
 	return (0);
