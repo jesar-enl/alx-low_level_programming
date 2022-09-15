441926ebff6bdbe4ecf43687b633ac2f1b36e437
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-15 05:50:54 -0700

    Print numbers on ten lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0-14 on ten lines
+ *
+ * Description: The function is printing numbers 0-14 on 10 different lines
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
