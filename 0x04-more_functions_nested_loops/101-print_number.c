b23cdebb9fe134d205cb7cc0b05c5b0dda8a57a5
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-15 09:02:35 -0700

    Prime numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,115 @@
 #include "main.h"
-void print_integer(int m);
+
+void print_two_digits(int);
+void print_three_digits(int);
+void print_four_digits(int);
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * print_number - prints numbers to the screen
+ * @n: Number to print
+ *
+ * Description: This function takes an integer,
+ * parses it and prints it to the terminal.
+ * Return: Void
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	if (n / 1000)
 	{
-		_putchar('-');
-		print_integer(n * -1);
+		print_four_digits(n);
+	}
+	else if (n / 100)
+	{
+		print_three_digits(n);
+	}
+	else if (n / 10)
+	{
+		print_two_digits(n);
 	}
 	else
-		print_integer(n);
+	{
+		_putchar('0' + n);
+	}
+}
+
+
+/**
+ * print_two_digits - prints two digit number to the screen
+ * @n: Number to be printed
+ *
+ * Return: void
+ */
+void print_two_digits(int n)
+{
+	int num1;
+	int num2;
+
+	num1 = n / 10;
+	num2 = n % 10;
+	if (n < 0)
+	{
+		num1 = -num1;
+		num2 = -num2;
+		_putchar('-');
+	}
+	_putchar('0' + num1);
+	_putchar('0' + num2);
 }
 
 /**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
+ * print_three_digits - prints three digit number to the screen
+ * @n: Number to be printed
+ *
+ * Return: void
  */
-void print_integer(int m)
+void print_three_digits(int n)
 {
-	int i = 1000000000;
+	int num1;
+	int num2;
+	int num3;
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+	num1 = n / 100;
+	num2 = (n % 100) / 10;
+	num3 = n % 10;
+	if (n < 0)
+	{
+		num1 = -num1;
+		num2 = -num2;
+		num3 = -num3;
+		_putchar('-');
+	}
+	_putchar('0' + num1);
+	_putchar('0' + num2);
+	_putchar('0' + num3);
+}
+
+/**
+ * print_four_digits - prints four digit number to the screen
+ * @n: Number to be printed
+ *
+ * Return: void
+ */
+void print_four_digits(int n)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	num1 = n / 1000;
+	num2 = ((n % 1000) / 100);
+	num3 = ((n % 100) / 10);
+	num4 = n % 10;
+	if (n < 0)
+	{
+		num1 = -num1;
+		num2 = -num2;
+		num3 = -num3;
+		num4 = -num4;
+		_putchar('-');
+	}
+	_putchar('0' + num1);
+	_putchar('0' + num2);
+	_putchar('0' + num3);
+	_putchar('0' + num4);
 }
