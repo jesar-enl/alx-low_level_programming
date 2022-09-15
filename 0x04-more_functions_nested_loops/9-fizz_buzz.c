39b0355a0230210f2cf76e080771acaea987b10f
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-15 08:35:30 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
@@ -21,7 +22,9 @@ int main(void)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
+		i++;
 	}
 	printf("\n");
+
 	return (0);
 }
