082439894dce28993a3625bb9be17b202d23a38d
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-19 07:55:48 -0700

    password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,73 +13,33 @@ void password_generator(int N)
 {
 	/* inititalize counter */
 	int p = 0;
-	int randomizer = 0;
-
+	
 	/* Seed the random number generator
 	 * with current time so that the
 	 * numbers will be different every time
 	 */
 	srand((unsigned int)(time(NULL)));
 
-	/* Array of numbers */
-	char numbers[] = "0123456789";
+	/* Array of numbers, lowercase, uppercase,and symbols */
+	char list[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$^&*?";
 
-	/* Array of small alphabets */
-	char letter[] = "abcdefghijklmnopqrstuvwxyz";
-
-	/* Array of uppercase letters */
-	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-	/* Array of special characters */
-	char symbols[] = "!@#$^&*?";
-
-	/* Variable to store the password */
-	char password[N];
-
-	/* randomizer inside the loop */
-	randomizer = rand() % 4;
-
-	/* iterate over the range */
 	for (; p < N; p++)
 	{
-		if (randomizer == 1)
-		{
-			password[p] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[p]);
-		}
-		else if (randomizer == 2)
-		{
-			password[p] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[p]);
-		}
-		else if (randomizer == 3)
-		{
-			password[p] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[p]);
-		}
-		else
-		{
-			password[p] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[p]);
-					}
-					}
-					}
+		printf("%c", list[rand() % (sizeof list - 1)]);
+	}
+	putchar('\n');
+}
+
 
 /**
- * main - driver code
+ * main - entry point
  *
  * Return: 0
  */
 int main(void)
 {
-	/* Length of password to be generated */
-	int N = 10;
-
+	int N = 15;
 	password_generator(N);
-	putchar('\n');
+
 	return (0);
 }
