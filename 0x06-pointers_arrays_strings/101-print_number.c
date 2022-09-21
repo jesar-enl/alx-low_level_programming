e467826e84c9085884e8779577bd7537fc9ff3c8
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-21 08:09:04 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,53 +1,112 @@
 #include "main.h"
 
-/**
- * base10 - converts the integer into base 10
- * @n: integer
- * Return: base
- */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-
-	return (base);
-}
+void print_two_digits(int);
+void print_three_digits(int);
+void print_four_digits(int);
 
 /**
- * print_number - prints the integer specified in base 10
- * @n: integer to be printed
+ * print_number - prints an integer to the screen
+ * @n: Integer to be printed to the screen
+ *
+ * Return: void
  */
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
-
-	if (n < 0)
+	if (n / 1000)
 	{
-		_putchar('-');
-		n *= -1;
+		print_four_digits(n);
 	}
-
-	if (n == 0)
+	else if (n / 100)
 	{
-		_putchar('0');
+		print_three_digits(n);
+	}
+	else if (n / 10)
+	{
+		print_two_digits(n);
 	}
 	else
 	{
-		while (n / power == 0)
-			power /= 10;
-
-		while (power >= 1)
+		if (n < 0)
 		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
+			n = -n;
+			_putchar('-');
 		}
+		_putchar('0' + n);
 	}
+}
+
+
+/**
+ * print_two_digits - prints two digit numbers to the screen
+ * @n: Two digit integer to print
+ *
+ * Return: void
+ */
+void print_two_digits(int n)
+{
+	int num1;
+	int num2;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	num1 = n % 10;
+	num2 = n / 10;
+	_putchar('0' + num2);
+	_putchar('0' + num1);
+}
+
+/**
+ * print_three_digits - prints three digit numbers to the screen
+ * @n: Three digit integer to print
+ *
+ * Return: void
+ */
+void print_three_digits(int n)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	num1 = n % 10;
+	num2 = (n % 100) / 10;
+	num3 = n / 100;
+	_putchar('0' + num3);
+	_putchar('0' + num2);
+	_putchar('0' + num1);
+}
+
+/**
+ * print_four_digits - prints four digit numbers to the screen
+ * @n: Four digit integer to print
+ *
+ * Return: void
+ */
+void print_four_digits(int n)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	num1 = n % 10;
+	num2 = (n % 100) / 10;
+	num3 = (n % 1000) / 100;
+	num4 = n / 1000;
+	_putchar('0' + num4);
+	_putchar('0' + num3);
+	_putchar('0' + num2);
+	_putchar('0' + num1);
 }
