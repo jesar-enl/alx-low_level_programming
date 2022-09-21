b2aa582d11c1ef7d97452634fd3390d722279a6c
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-21 10:22:29 -0700

    Reverse order

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,15 +13,12 @@ void reverse_array(int *a, int n)
 
 	i = 0;
 	j = n - 1;
-	while (i < j)
+	for (;i < j; i++, j--)
 	{
 		int temp;
 
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
-
-		i++;
-		j++;
 	}
 }
