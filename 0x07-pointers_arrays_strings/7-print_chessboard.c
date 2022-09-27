88f252980e5913f0aed529f0c22fddb06de543d5
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-09-26 20:27:36 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	/*Declaring Variables*/
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	i = 0;
+	while (i < 8) /*Repetition*/
 	{
-		for (j = 0; j < 8; j++ )
+		j = 0;
+		while (j < 8)
 		{
 			_putchar(a[i][j]);
+			j++;
 		}
-		_putchar('\n');
+		_putchar('\n'); /*Print Newline*/
+		i++; /*i + 1*/
 	}
 }
