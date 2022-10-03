0add3266dfa8ab2e649c9d7ef3e139cf13a31112
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-03 08:22:17 -0700

    Allocate memory and free it

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * alloc_grid - pointer to a 2D array of ints
+ * @width: rows
+ * @height: columns
+ * Return: array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (width <= 0|| height <= 0)
+		return (NULL);
+
+	arr = (int **) malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
