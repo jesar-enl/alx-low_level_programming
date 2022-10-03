abb949cb95a43169d0d0d38567134efed7b52578
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-03 08:29:50 -0700

    Allocate and free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,13 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
-			return (NULL);
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+		}
 	}
 
 	for (i = 0; i < height; i++)
