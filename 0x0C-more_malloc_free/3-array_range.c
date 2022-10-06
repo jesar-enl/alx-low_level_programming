7b795ae2ffe024489804b31e073e8126a676a760
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-06 07:38:11 -0700

    Array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,10 +11,11 @@ int *array_range(int min, int max)
 	int i, size = (max - min ) + 1;
 	int *p;
 
+	if (min > max)
+		return (NULL);
+
 	p = (int *) malloc(sizeof(int) * size);
 	if (p == NULL)
-		return (NULL);
-	if (min > max)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
