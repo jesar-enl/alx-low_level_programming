7519f41653aa8f259cd0576b3ce3b21adc09982d
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-20 07:06:48 -0700

    Find the length of the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 
+/**
+ * list_len - lenght of the list
+ * @h: points to node
+ * Return: number of nodes
+ */
 size_t list_len(const list_t *h)
 {
-	unsigned int count = 0;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		i++;
 		h = h->next;
 	}
-	return (count);
+	return (i);
 }
