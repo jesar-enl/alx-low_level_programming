2e5d6b21df0470e89960b8ec5faff9008a02509b
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-25 02:10:05 -0700

    Add new node at index x

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a new node at
+ * a particular index
+ * @head: start point of the list
+ * @idx: position being inserted
+ * @n: data
+ * Return: address of the new node
+ */
