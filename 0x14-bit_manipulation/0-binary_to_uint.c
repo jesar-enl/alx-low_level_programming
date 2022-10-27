55cab2894adb7a8baf4c2490f51284b1c13a8de0
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2022-10-27 14:34:06 +0300

    binary conversion to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * @b: points to a string of 0's and 1's
  * Return: decimal format of the binary
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0; /* dec is the variable to hold the decimal format */
 
