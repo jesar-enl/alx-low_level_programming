42124d9b3f973b77b4b5918181477ab21e74b474
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-10-27 05:08:34 -0700

    Print the binary format

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - converts the decimal format number into binary
+ * @n: decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	else if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
