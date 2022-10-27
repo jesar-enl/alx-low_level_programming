a4cd995c0639474dd9a5e3a498410b4c5dec9ad4
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2022-10-27 15:10:31 +0300

    Print binary format

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,10 +6,19 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	else if (n & 1)
-		_putchar('1');
-	else
+	int temp;
+	static int count;
+
+	if (n == 0 && count > 0)
+		return;
+	else if (n == 0)
+	{
 		_putchar('0');
+		return;
+	}
+
+	temp = (n & 1);
+	count++;
+	print_binary(n >>= 1);
+	_putchar('0' + temp);
 }
