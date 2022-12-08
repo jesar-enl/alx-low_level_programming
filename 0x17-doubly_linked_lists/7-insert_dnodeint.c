ac4f8be8550b239fff422afe3587e62f317028e6
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-12-08 17:53:55 +0300

    Insert a new node a specified position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,35 +9,35 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        dlistint_t *temp, *temp2;
-        unsigned int i = 0;
+	dlistint_t *temp, *temp2;
+	unsigned int i = 0;
 
-        if (*h == NULL)
-        {
-                return (NULL);
-        }
-        temp2 = malloc(sizeof(dlistint_t));
-        if (temp2 == NULL)
-        {
-                return (NULL);
-        }
-        temp2->n = n;
-        temp2->next = NULL;
-        temp2->prev = NULL;
-        temp = *h;
-        while (temp != NULL)
-        {
-                if (i == idx)
-                {
-                        (temp->prev)->next = temp2;
-                        temp2->prev = temp->prev;
-                        temp->prev = temp2;
-                        temp2->next = temp;
-                        return (temp2);
-                }
-                temp = temp->next;
-                i++;
-        }
-        free(temp2);
-        return (NULL);
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
+	temp2 = malloc(sizeof(dlistint_t));
+	if (temp2 == NULL)
+	{
+		return (NULL);
+	}
+	temp2->n = n;
+	temp2->next = NULL;
+	temp2->prev = NULL;
+	temp = *h;
+	while (temp != NULL)
+	{
+		if (i == idx)
+		{
+			(temp->prev)->next = temp2;
+			temp2->prev = temp->prev;
+			temp->prev = temp2;
+			temp2->next = temp;
+			return (temp2);
+		}
+		temp = temp->next;
+		i++;
+	}
+	free(temp2);
+	return (NULL);
 }
