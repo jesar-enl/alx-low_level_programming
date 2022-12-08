f2e2f8a292f5e9ddd8bcc722fede7e18a6e64d53
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-12-08 17:34:06 +0300

    Insert a new node a specified position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *temp2;
 	unsigned int i = 0;
 
-	/* if the head pointer is NULL, return NULL*/
 	if (*h == NULL)
 	{
 		return (NULL);
 	}
-	/* Allocate memory to the new pointer */
 	temp2 = malloc(sizeof(dlistint_t));
 	if (temp2 == NULL)
 	{
@@ -27,16 +25,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp2->next = NULL;
 	temp2->prev = NULL;
 	temp = *h;
-
-	/* If the previous pointer is NULL */
 	while (temp != NULL)
 	{
-		/* If the index is equal to the position specified */
 		if (i == idx)
 		{
 			(temp->prev)->next = temp2;
 			temp2->prev = temp->prev;
-			temp->prev = temp;
+			temp->prev = temp2;
 			temp2->next = temp;
 			return (temp2);
 		}
